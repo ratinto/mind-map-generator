08598b3022ad5ba34299d51237de4e4d3be98607
Ritesh Kumar <7976ritesh@gmail.com>
2025-05-07 02:47:10 +0530

    tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState } from "react";
 import ReactFlow, {
   MiniMap,
   Controls,
@@ -6,31 +6,30 @@ import ReactFlow, {
   addEdge,
   useNodesState,
   useEdgesState,
-} from 'reactflow'
-import 'reactflow/dist/style.css'
+} from "reactflow";
+import "reactflow/dist/style.css";
 
-
-let id = 0
-const getId = () => `node_${id++}`
+let id = 0;
+const getId = () => `node_${id++}`;
 
 export default function App() {
   const initialNodes = [
     {
       id: getId(),
-      type: 'default',
-      data: { label: 'Main Topic' },
+      type: "default",
+      data: { label: "Main Topic" },
       position: { x: 250, y: 5 },
     },
-  ]
+  ];
 
-  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
-  const [edges, setEdges, onEdgesChange] = useEdgesState([])
-  const [nodeText, setNodeText] = useState('')
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [nodeText, setNodeText] = useState("");
 
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
     [setEdges]
-  )
+  );
 
   const addNode = () => {
     const newNode = {
@@ -40,24 +39,13 @@ export default function App() {
         x: Math.random() * 250,
         y: Math.random() * 250,
       },
-    }
-    setNodes((nds) => [...nds, newNode])
-  }
+    };
+    setNodes((nds) => [...nds, newNode]);
+  };
 
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <input
-        type="text"
-        onChange={(e) => setNodeText(e.target.value)}
-        className='bg-red-100'
-      />
-      <button
-  onClick={addNode}
-  className="absolute top-2 left-2 z-10 bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600 transition"
->
-  Add Node
-</button>
-
+    <div style={{ width: "100vw", height: "100vh" }}>
+      
 
       <ReactFlow
         nodes={nodes}
@@ -68,10 +56,21 @@ export default function App() {
         fitView
         proOptions={{ hideAttribution: true }}
       >
+        <input
+        type="text"
+        onChange={(e) => setNodeText(e.target.value)}
+        className="bg-red-100"
+      />
+      <button
+        onClick={addNode}
+        className="absolute top-2 left-2 z-10 bg-blue-500 text-white px-4 py-1 rounded hover:bg-blue-600 transition"
+      >
+        Add Node
+      </button>
         <MiniMap />
         <Controls />
         <Background />
       </ReactFlow>
     </div>
-  )
+  );
 }
